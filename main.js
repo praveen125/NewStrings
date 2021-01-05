c7a1b4e469e28f7b1db7aab2faa9dfa1b2ced9a1
praveen125 <praveensurisetti0@gmail.com>
2021-01-05 16:16:51 +0530

    commit

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let theString = "Hello , my name is Brad and I love javaScript";
+
+//startsWith()
+//endsWith()
+//includes()
+
+// console.log(theString.startsWith("Hello"));
+// console.log(theString.endsWith("javaScript"));
+// console.log(theString.includes("love"));
+
+//Hex
+// console.log(0xff);
+//Binary
+// console.log(0b101011);
+//octal
+// console.log(0o543);
+
+// console.log(Number.isFinite(3));
+// console.log(Number.isNaN(NaN));
+// console.log(Number.isInteger(3));
